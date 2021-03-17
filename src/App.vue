9363d9ba816c8c4f3c5f1cd32087abfa48ac52b8
Prosenjit Kundu <prosenkuet10@gmail.com>
2021-03-17 20:02:14 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Product List</router-link> | <a @click="logout">Logout</a>
+      <span v-if="isLoggedIn"> <a @click="logout">Logout</a> | <router-link to="/">Product List</router-link></span>
     </div>
     <router-view/>
   </div>
