d9e7b03b119d110d0dedcb0e5a0d07d1335c0916
Prosenjit <prosenkuet10@gmail.com>
2021-03-17 09:55:29 +0600

    initialize vue spa

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from './store.js'
+
+Vue.use(Router)
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+  ]
+})
+
+
+
+export default router
