8e8a95daa5b08aa9b1646fa06cec65c1533042ca
Prosenjit <prosenkuet10@gmail.com>
2021-03-17 10:15:36 +0600

    Register authentication

--- src/router.js
+++ src/router.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import store from './store.js'
+import Register from './components/Register.vue'
+
 
 Vue.use(Router)
 
 let router = new Router({
   mode: 'history',
   routes: [
+  {
+      path: '/register',
+      name: 'register',
+      component: Register
+  },
   ]
 })
 
--- src/store.js
+++ src/store.js
@@ -5,8 +5,52 @@ import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
-	getters : {}
+	state: {
+		status: '',
+		token: localStorage.getItem('token') || '',
+		user : {}
+	},
+	mutations: {
+		auth_request(state){
+			state.status = 'loading'
+		},
+		auth_success(state, token, user){
+			state.status = 'success'
+			state.token = token
+			state.user = user
+		},
+		auth_error(state){
+			state.status = 'error'
+		},
+		logout(state){
+			state.status = ''
+			state.token = ''
+		},
+	},
+	actions: {
+		register({commit}, user){
+			return new Promise((resolve, reject) => {
+				commit('auth_request')
+				axios({url: 'http://127.0.0.1:8000/api/register', data: user, method: 'POST' })
+					.then(resp => {
+						const token = resp.data.token
+						const user = resp.data.user
+						localStorage.setItem('token', token)
+						// Add the following line:
+						axios.defaults.headers.common['Authorization'] = token
+						commit('auth_success', token, user)
+						resolve(resp)
+					})
+					.catch(err => {
+						commit('auth_error', err)
+						localStorage.removeItem('token')
+						reject(err)
+					})
+			})
+		},
+	},
+	getters : {
+		isLoggedIn: state => !!state.token,
+		authStatus: state => state.status,
+	}
 })
