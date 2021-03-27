141834dffeb49049123883072949661fe0457892
Prosenjit <prosenkuet10@gmail.com>
2021-03-27 17:04:53 +0600

    consoled token

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ Vue.prototype.$http = Axios;
 
 const token = localStorage.getItem('user-token')
 if (token) {
+  console.log(token);
   Vue.prototype.$http.defaults.headers.common['Authorization'] = token
 }
 
